3df8d5f2719b029f61bc03c1e5cba8465a5edf8b
yochiba <yoshikazu.oshiba@gmail.com>
2020-12-31 00:44:26 +0900

    develop_1_34

--- api_server/app/controllers/api/v1/projects_controller.rb
+++ api_server/app/controllers/api/v1/projects_controller.rb
@@ -5,14 +5,14 @@ class Api::V1::ProjectsController < ApplicationController
   # GET index
   def index
     # search_query = compose_search_query params
-    pjt_json = ProjectService.project_list_json params
-    res_json = ResponseService.pjts_list_search_result_json pjt_json
+    pjt_list_json = ProjectService.project_list_json params
+    res_json = ResponseService.pjts_list_search_result_json pjt_list_json
     render json: res_json
   end
 
   # GET show
   def show
-    pjt_json = ProjectService.project_json(params[:id])
+    pjt_json = ProjectService.project_json(params[:pjtId])
     res_json = ResponseService.pjt_json pjt_json
     render json: res_json
   end
