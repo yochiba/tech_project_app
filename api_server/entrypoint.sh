224c9c2b864b865b080546ac1ff7dcc8a74fd304
yochiba <yoshikazu.oshiba@gmail.com>
2021-01-20 01:13:17 +0900

    develop_1_42

---
+++ api_server/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+rm -f /tech_project_app/tmp/pids/server.pid
+
+touch ~/.bash_profile
+echo "export CORS_ALLOWED_ORIGINS_HOST='http://localhost:3000'" >> ~/.bash_profile
+echo "export MYSQL_USERNAME='root'" >> ~/.bash_profile
+echo "export MYSQL_PASSWORD='test'" >> ~/.bash_profile
+source ~/.bash_profile
+
+cd /var/www/html/app/tech_project_app/api_server/config/
+# TODO change the command after -e depends on environment
+EDITOR="vi" rails credentials:edit -e development
+cd ../
+# TODO change the command after RAILS_ENV= depends on environment
+rails db:create RAILS_ENV=development
+rails db:migrate RAILS_ENV=development
+
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+exec "$@"
